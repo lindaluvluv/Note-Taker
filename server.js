4b58ea299f867261e211dc0d8cfa8cee47520aa3
Linda Fan <f.linda97@gmail.com>
2021-10-21 09:06:33 -0700

    abc

--- server.js
+++ server.js
@@ -1,41 +1,44 @@
-const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const express = require("express");
+
 const database = require("./public/db/db.json")
 var app = express();
-var PORT = process.env.PORT || 3001
+var PORT = process.env.PORT || 3002
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static('./'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get("/", function (req, res) {
+app.get ("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/notes", function (req, res) {
+app.get ("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
-app.route("/api/notes")
-    .get(function (req, res) {
+app.route ("/api/notes")
+    .get (function (req, res) {
         res.json(database);
     })
 
-    .post(function (req, res) {
-        let jsonFilePath = path.join(__dirname, "/db/db.json");
+    .post (function (req, res) {
+        let jsonFilePath = path.join (__dirname, "/db/db.json");
         let newNote = req.body;
 
-        let highestId = 99;
+        let biggestId = 99;
         for (let i = 0; i < database.length; i++) {
-            let individualNote = database[i];
+            let oneNote = database[i];
 
-            if (individualNote.id > highestId) {
-                highestId = individualNote.id;
+            if (oneNote.id > biggestId) {
+                biggestId = oneNote.id;
             }
         }
-        newNote.id = highestId + 1;
+        newNote.id = biggestId + 1;
         database.push(newNote)
 
         fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
@@ -43,7 +46,7 @@ app.route("/api/notes")
             if (err) {
                 return console.log(err);
             }
-            console.log("Notes saved!");
+            console.log("Note Successfully Saved!");
         });
         res.json(newNote);
     });
@@ -63,7 +66,7 @@ app.delete("/api/notes/:id", function (req, res) {
         if (err) {
             return console.log(err);
         } else {
-            console.log("Successfully deleted!");
+            console.log("Note Successfully deleted!");
         }
     });
     res.json(database);
